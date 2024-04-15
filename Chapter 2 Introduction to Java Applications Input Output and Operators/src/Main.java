65ee3f84b975d5a3b77bc86fb2d40b0188626c92
Jose Valim <joseaugustovalim1994@gmail.com>
2024-04-15 20:50:20 -0300

    feat: Add chapter 2 and the exercise 15

---
+++ Chapter 2 Introduction to Java Applications Input Output and Operators/src/Main.java
@@ -0,0 +1,9 @@
+// Displaying with printf
+// F is formatted
+// call method is the same to invoke
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n", "Welcome to","Java Programming");
+    }
+}

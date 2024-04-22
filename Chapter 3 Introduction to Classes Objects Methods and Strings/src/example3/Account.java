5dd06179a72f33a8aafcfa2d93bd0fb0ada53a44
Jose Valim <joseaugustovalim1994@gmail.com>
2024-04-22 17:44:39 -0300

    Fix: Account class with deposit and balance

--- Chapter 3 Introduction to Classes Objects Methods and Strings/src/example3/Account.java
+++ Chapter 3 Introduction to Classes Objects Methods and Strings/src/example3/Account.java
@@ -2,18 +2,31 @@ package example3;
 
 public class Account {
     private String name; // instance variable
+    private double balance; // instance variable
 
-    // constructor initializes name with parameter name
-    public Account(String name) {  //constructor name is class name
-        this.name = name;
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
-        this.name = name; // store the name
+        this.name = name;
     }
 
-    // method to retrieve the name from the object
-    public String getName() {
-        return name; // return value of name to caller
+    public double getBalance() {
+        return balance;
     }
+
+    public Account(String name, double balance){
+        this.name = name;
+        if (balance > 0.0){
+            this.balance = balance;
+        }
+    }
+
+    public void deposit(double depositAmount){
+        if (depositAmount > 0.0){
+            balance = balance + depositAmount;
+        }
+    }
+
 }
--- Chapter 3 Introduction to Classes Objects Methods and Strings/src/example3/AccountTest.java
+++ Chapter 3 Introduction to Classes Objects Methods and Strings/src/example3/AccountTest.java
@@ -1,13 +1,34 @@
 package example3;
 
+import java.util.Scanner;
+
 public class AccountTest {
     public static void main(String[] args) {
         // create two account object
-        Account account1 = new Account("Jose Valim");
-        Account account2 = new Account("Olga Santos");
+        Account account1 = new Account("Jose Valim", 50.00);
+        Account account2 = new Account("Olga Santos", -7.53);
 
-        // display initial value of name (null)
-        System.out.printf("account1 name is: %s%n", account1.getName());
-        System.out.printf("account2 name is: %s%n", account2.getName());
+        System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f%n", account2.getName(), account2.getBalance());
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter deposit amount for account1: ");
+        double depositAmount = input.nextDouble();
+        System.out.printf("%nadding %.2f to account1 balance%n%n", depositAmount);
+        account1.deposit(depositAmount);
+
+        // display balances
+        System.out.printf("%s balance: $%.2f%n",account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f%n",account2.getName(), account2.getBalance());
+
+        System.out.print("Enter deposit amount for account2: ");
+        depositAmount = input.nextDouble();
+        System.out.printf("%nadding $%.2f to account2 balance%n%n",depositAmount);
+        account2.deposit(depositAmount);
+
+        System.out.printf("%s balance: $%.2f%n",account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f%n",account2.getName(), account2.getBalance());
+
     }
 }

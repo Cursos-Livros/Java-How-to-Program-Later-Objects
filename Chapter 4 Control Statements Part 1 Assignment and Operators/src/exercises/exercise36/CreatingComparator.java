735eb6d0d735bfa5465c9babedb4804569d0dbb6
Jose Valim <joseaugustovalim1994@gmail.com>
2024-06-13 17:38:32 -0300

    feat: add exercise 36

---
+++ Chapter 4 Control Statements Part 1 Assignment and Operators/src/exercises/exercise36/CreatingComparator.java
@@ -0,0 +1,25 @@
+package exercises.exercise36;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CreatingComparator {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number 1:");
+        int number1 = input.nextInt();
+
+        System.out.println("Enter the number 2:");
+        int number2 = input.nextInt();
+
+        if (number1 == number2) {
+            System.out.println(0);
+        } else if (number1 > number2) {
+            System.out.println(1);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}

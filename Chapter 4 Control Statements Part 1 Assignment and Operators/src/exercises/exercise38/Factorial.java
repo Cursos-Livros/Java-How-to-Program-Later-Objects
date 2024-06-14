df6f80357f4d434901d40b333cdfbc4f8404bf2a
Jose Valim <joseaugustovalim1994@gmail.com>
2024-06-14 10:59:27 -0300

    feat: finalizado exercise 38

--- Chapter 4 Control Statements Part 1 Assignment and Operators/src/exercises/exercise38/Factorial.java
+++ Chapter 4 Control Statements Part 1 Assignment and Operators/src/exercises/exercise38/Factorial.java
@@ -21,6 +21,36 @@ public class Factorial {
             fatorial *= i;
         }
 
+
+        double constantE = 1;
+
+        for (int i = 1; i <= number; i++) {
+            // Reinicializa o fatorial a cada iteracao para ficar
+            long fatorialb = 1;
+            // Calcula o fatorial
+            for (int j = 1; j <= i; j++) {
+                fatorialb *= j;
+            }
+            constantE +=  (1.0 / fatorialb);
+        }
+
+        System.out.println("Enter the non negative number:");
+        int x = input.nextInt();
+
+        double constantEPow = 1;
+
+        for (int i = 1; i <= number; i++) {
+            // Reinicializa o fatorial a cada iteracao para ficar
+            long fatorialb = 1;
+            // Calcula o fatorial
+            for (int j = 1; j <= i; j++) {
+                fatorialb *= j;
+            }
+            constantEPow +=  (Math.pow(x, i) / fatorialb);
+        }
+
         System.out.println(fatorial);
+        System.out.println(constantE);
+        System.out.println(constantEPow);
     }
 }

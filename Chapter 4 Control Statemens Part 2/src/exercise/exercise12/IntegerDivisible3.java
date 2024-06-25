cbfc18908803760583ab55ca547449dd96561b13
Jose Valim <joseaugustovalim1994@gmail.com>
2024-06-24 21:53:58 -0300

    feat: Exercise 12

--- Chapter 4 Control Statemens Part 2/src/exercise/exercise12/IntegerDivisible3.java
+++ Chapter 4 Control Statemens Part 2/src/exercise/exercise12/IntegerDivisible3.java
@@ -1,7 +1,18 @@
 package exercise.exercise12;
 
+import java.util.Scanner;
+
 public class IntegerDivisible3 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter number:");
+        int number = input.nextInt();
+
+        if (number % 3 == 0) {
+            System.out.println("Is divisible!");
+        }else{
+            System.out.println("Is not divisible!");
+        }
     }
 }

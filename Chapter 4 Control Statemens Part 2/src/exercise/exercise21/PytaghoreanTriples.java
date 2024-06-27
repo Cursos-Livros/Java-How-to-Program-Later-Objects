bbb7563870832f4f5d1cc242d8b492106e07c7b2
Jose Valim <joseaugustovalim1994@gmail.com>
2024-06-27 16:35:47 -0300

    feat: exercise 21

--- Chapter 4 Control Statemens Part 2/src/exercise/exercise21/PytaghoreanTriples.java
+++ Chapter 4 Control Statemens Part 2/src/exercise/exercise21/PytaghoreanTriples.java
@@ -1,2 +1,19 @@
-package exercise.exercise21;public class PytaghoreanTriples {
+package exercise.exercise21;
+
+public class PytaghoreanTriples {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        for (int i = 1; i <= 500; i++) {
+            for (int j = 1; j <= 500; j++) {
+                for (int k = 1; k <= 500; k++) {
+                    if (Math.pow(i, 2.0) + Math.pow(j, 2.0) == Math.pow(k, 2.0)) {
+                        System.out.println(i + " " + j + "=  " + k);
+                    }
+                }
+            }
+        }
+    }
 }

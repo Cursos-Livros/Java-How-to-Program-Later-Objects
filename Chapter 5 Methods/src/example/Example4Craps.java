0f813c0cfe931b7bc6c67bdc8c16a8aa26430d20
Jose Valim <joseaugustovalim1994@gmail.com>
2024-07-08 15:04:25 -0300

    feat: example 4 craps finished

--- Chapter 5 Methods/src/example/Example4Craps.java
+++ Chapter 5 Methods/src/example/Example4Craps.java
@@ -6,6 +6,7 @@ public class Example4Craps {
     // create secure random number generator for use in method rollDice
     private static final SecureRandom randomNumbers = new SecureRandom();
 
+    // Tipo especial da classe para declarar varia constantes
     // enum type with constants that represent the game status
     private enum Status {CONTINUE, WON, LOST};
 
@@ -19,27 +20,64 @@ public class Example4Craps {
     // plays one game of craps
     public static void main(String[] args) {
         // constants that represent common rolls of the dice
-        int myPoint  = 0;
+        int myPoint = 0;
         Status gameStatus;
 
-       // int sumOfDice = rollDice();
+        int sumOfDice = rollDice();
 
-//        switch (sumOfDice){
-//            case SEVEN: // win with 7 on first roll
-//            case YO_LEVEN:  // win with 11 on first roll
-//                gameStatus = Status.WON;
-//                break;
-//            case SNAKE_EYES: // lose with 2 on first roll
-//            case TREY: // lose with 3 on first roll
-//            case BOX_CARS: // lose with 12 on first roll
-//                gameStatus = Status.LOST;
-//                break;
-//            default: // did not win or lose, so remember point
-//                gameStatus = Status.CONTINUE; // game is not over
-//                myPoint = sumOfDice; // remember the point
-//                System.out.printf("Point is %d%n", myPoint);
-//                break;
+        // Para testar a primeira jogada
+        switch (sumOfDice) {
+            case SEVEN: // win with 7 on first roll
+            case YO_LEVEN:  // win with 11 on first roll
+                gameStatus = Status.WON;
+                break;
+            case SNAKE_EYES: // lose with 2 on first roll
+            case TREY: // lose with 3 on first roll
+            case BOX_CARS: // lose with 12 on first roll
+                gameStatus = Status.LOST;
+                break;
+            default: // did not win or lose, so remember point
+                gameStatus = Status.CONTINUE; // game is not over
+                myPoint = sumOfDice; // remember the point
+                System.out.printf("Point is %d%n", myPoint);
+                break;
+        }
 
+        // while game is not complete
+        while (gameStatus == Status.CONTINUE) { // not WON or LOST
+            sumOfDice = rollDice(); // roll dice again
+
+            // determine game status
+            if (sumOfDice == myPoint) { // win by making point
+                gameStatus = Status.WON;
+            }
+            if (sumOfDice == SEVEN) { // lose by rolling 7 before point
+                gameStatus = Status.LOST;
+            }
+        }
+
+        // display won or lost message
+        if ( gameStatus == Status.WON) {
+            System.out.println("Player wins");
+        }
+        else {
+            System.out.println("Player loses");
         }
     }
+
+    // roll dice, calculate sum and display results
+    public static int rollDice(){
+        // pick random die values
+        int die1 = 1 + randomNumbers.nextInt(6);
+        int die2 = 1 + randomNumbers.nextInt(6);
+
+        int sum = die1 + die2; // sum of die values
+
+        // display results of this roll
+
+        System.out.printf("Player rolled %d + %d = %d%n", die1, die2, sum);
+        return sum;
+    }
+}
+
 

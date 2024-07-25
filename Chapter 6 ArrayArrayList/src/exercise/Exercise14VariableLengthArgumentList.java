bca27ad56d7bb75eb9d636d99fb723eba29dd6ce
Jose Valim <joseaugustovalim1994@gmail.com>
2024-07-25 16:36:14 -0300

    feat: Chapter 6 exercise 14 Variable Length ArgumentList

--- Chapter 6 ArrayArrayList/src/exercise/Exercise14VariableLengthArgumentList.java
+++ Chapter 6 ArrayArrayList/src/exercise/Exercise14VariableLengthArgumentList.java
@@ -1,2 +1,42 @@
-package exercise;public class Exercise14VariableLengthArgumentList {
+package exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise14VariableLengthArgumentList {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What the quantity of number you'll pass:");
+        Integer quantityNumbers = input.nextInt();
+
+        Integer[] arrayNumber = fillList(quantityNumbers, input);
+
+        System.out.println("That's your list:");
+        System.out.println(Arrays.toString(arrayNumber));
+
+        System.out.println("Average: " + calculateAverage(arrayNumber));
+
+        input.close();
+    }
+
+    public static Integer[] fillList(int size, Scanner input) {
+        Integer[] listCalculate = new Integer[size];
+
+        for (int i = 0; i < listCalculate.length; i++) {
+            listCalculate[i] = input.nextInt();
+        }
+
+        return listCalculate;
+    }
+
+    public static Double calculateAverage(Integer[] arrayNumber) {
+        double average = 0.0;
+
+        for (Integer integer : arrayNumber) {
+            average += integer.doubleValue();
+        }
+
+        return average / arrayNumber.length;
+    }
 }

d9020d3bfd1e8d453d8cea23a230ca9d19c0102c
Jose Valim <joseaugustovalim1994@gmail.com>
2024-08-05 09:30:27 -0300

    feat: example 3 Account Balance

---
+++ Chapter 7 Introduction Class Object/src/example3ClassAccountBalance/Account.java
@@ -0,0 +1,47 @@
+package example3ClassAccountBalance;
+
+/*
+  Variaveis de Instancia
+  Mantem os dados de cada instancia (ocorrencia daquela classe)
+
+* Modificador de Acesso
+*
+  Encapsulamento
+  O acesso as variaveis de intancia sao feitos a partir de metodos
+  nao se deve dar acesso direto as variaveis de instancia
+* */
+
+public class Account {
+    private String name;
+    private double balance;
+
+    // construtor que inicializa name atraves do parametro name
+    public Account(String name, double balance) {
+        this.name = name;
+        if (balance > 0.0) {
+            this.balance = balance;
+        }
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double depositAmount) {
+        if (depositAmount > 0.0) {
+            balance += depositAmount;
+        }
+    }
+}
---
+++ Chapter 7 Introduction Class Object/src/example3ClassAccountBalance/AccountTest.java
@@ -0,0 +1,47 @@
+package example3ClassAccountBalance;
+
+import java.util.Scanner;
+
+/*
+    Construtores
+    construtores nao retornam valor
+    normalmente sao declarados como publicos
+    constutores padroes sao declarados de forma implicita na criacao de metodo
+    Exemplo
+    Scanner input = new Scanne(); -> () declara um construtor padrao inplicito vazio ouseja null
+    Nao existe construtor padrao caso tenhamos o nosso proprio
+
+ */
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // cria 2 objetos Account
+        Account account1 = new Account("Jane Green", 50.00);
+        Account account2 = new Account("Jhon Blue", -7.53);
+
+        // display initial balance de cada objeto
+        System.out.printf("%s balance: $ %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $ %n", account1.getName(), account2.getBalance());
+
+        // Display saldo
+        System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f%n", account2.getName(), account2.getBalance());
+
+        System.out.print("Enter deposit amount for account1: ");
+        double depositAmount = input.nextDouble(); // obtain user input
+        System.out.printf("%nadding to account1 balance%n%n", depositAmount);
+        account1.deposit(depositAmount);
+
+        System.out.print("Enter deposit amount for account2: ");
+        depositAmount = input.nextDouble(); // obtain user input
+        System.out.printf("%nadding to account2 balance%n%n", depositAmount);
+        account2.deposit(depositAmount);
+
+        // mostrar os saldos
+        System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n", account1.getName(), account2.getBalance());
+
+    }
+}

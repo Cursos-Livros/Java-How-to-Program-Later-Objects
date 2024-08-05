1f5d95d8e5178b314ecdb5d86cd40b88f40ae2b1
Jose Valim <joseaugustovalim1994@gmail.com>
2024-08-05 08:54:49 -0300

    feat: add account class

---
+++ Chapter 7 Introduction Class Object/src/example/example1account/Account.java
@@ -0,0 +1,21 @@
+package example.example1account;
+
+/*
+  Variaveis de Instancia
+  Mantem os dados de cada instancia (ocorrencia daquela classe)
+
+* Modificador de Acesso
+*
+* */
+
+public class Account {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

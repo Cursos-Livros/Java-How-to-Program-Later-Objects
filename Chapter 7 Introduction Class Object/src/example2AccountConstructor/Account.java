5e5141cbea611866cbf98674b63b1e0bd82805f5
Jose Valim <joseaugustovalim1994@gmail.com>
2024-08-05 09:01:27 -0300

    feat: account class constructor

---
+++ Chapter 7 Introduction Class Object/src/example2AccountConstructor/Account.java
@@ -0,0 +1,29 @@
+package example2AccountConstructor;
+
+/*
+  Variaveis de Instancia
+  Mantem os dados de cada instancia (ocorrencia daquela classe)
+
+* Modificador de Acesso
+*
+  Encapsulamento
+  O acesso as variaveis de intancia sao feitos a partir de metodos
+  nao se deve dar acesso direto as variaveis de instancia
+* */
+
+public class Account {
+    private String name;
+
+    // construtor que inicializa name atraves do parametro name
+    public Account(String name){
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Chapter 7 Introduction Class Object/src/example2AccountConstructor/AccountTest.java
@@ -0,0 +1,25 @@
+package example2AccountConstructor;
+
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+
+        // cria um objeto account e atribui a myAccount
+        Account myAccount =  new Account();
+
+        // display o valor inicial como null
+        System.out.printf("Initial name is %s:%n", myAccount.getName());
+
+        // prompt for e de leitura
+        System.out.println("Please enter the name: ");
+        String name = input.nextLine();
+        myAccount.setName(name);
+        System.out.println();
+
+        // mostra o nome guardado no objeto myAccount
+        System.out.printf("Name in object myAccount is:%n%s%n",myAccount.getName());
+
+    }
+}

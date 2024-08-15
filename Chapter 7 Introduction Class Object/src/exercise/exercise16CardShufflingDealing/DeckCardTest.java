0a43e312d9c7e594d5f1b939745a8fa5c865756a
Jose Valim <joseaugustovalim1994@gmail.com>
2024-08-15 17:01:50 -0300

    fix: changing the way of deal the cards

--- Chapter 7 Introduction Class Object/src/exercise/exercise16CardShufflingDealing/DeckCardTest.java
+++ Chapter 7 Introduction Class Object/src/exercise/exercise16CardShufflingDealing/DeckCardTest.java
@@ -1,19 +1,24 @@
 package exercise.exercise16CardShufflingDealing;
 
+import java.util.Arrays;
+
 public class DeckCardTest {
     public static void main(String[] args) {
         DeckCards myDeckCards = new DeckCards();
+        Card[] hand = new Card[5];
 
+        myDeckCards.shuffle();
+
+        for (int numberCards = 0; numberCards < 5; numberCards++) {
+            hand[numberCards] = myDeckCards.deal();
+        }
         // Mostra todas as 52 posicoes do baralho embaralhado
-        showDeck(myDeckCards);
+        showDeck(hand);
     }
 
-    public static void showDeck(DeckCards deckCards) {
-        for (int i = 1; i <= 52; i++) {
-            System.out.printf("%-19s", deckCards.deal());
-            if (i % 4 == 0) { // output a newline after every fourth card
-                System.out.println();
-            }
+    public static void showDeck(Card[] hand) {
+        for (int i = 0; i <= hand.length - 1; i++) {
+            System.out.println("hand = " + hand[i]);
         }
     }
 }

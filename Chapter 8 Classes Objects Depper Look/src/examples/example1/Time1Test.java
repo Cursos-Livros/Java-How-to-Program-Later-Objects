c605f5cbdd40303298e354c979e2ade78f9ec82d
Jose Valim <joseaugustovalim1994@gmail.com>
2024-08-27 16:05:12 -0300

    feat: Primeiro exemplo capitulo 8

---
+++ Chapter 8 Classes Objects Depper Look/src/examples/example1/Time1Test.java
@@ -0,0 +1,26 @@
+package examples.example1;
+
+public class Time1Test {
+    public static void main(String[] args) {
+        Time1 time = new Time1();
+
+        displayTime("After time object is created", time);
+
+        time.setTime(13, 27, 6);
+        displayTime("After calling setTime", time);
+        System.out.println();
+
+        // Tratamos o erro IllegalArgumentException lancado pela classe caso o horario estiesse em um intervalo nao
+        // permitido
+        try {
+            time.setTime(99, 99, 99);
+        } catch (IllegalArgumentException e) {
+            System.out.printf("Exception: %s%n%n", e.getMessage());
+        }
+    }
+
+    public static void displayTime(String header, Time1 time) {
+        System.out.printf("%s%nUniversal time: %s%nStandard time: %s%n", header, time.toUniversalString(),
+                time.toString());
+    }
+}

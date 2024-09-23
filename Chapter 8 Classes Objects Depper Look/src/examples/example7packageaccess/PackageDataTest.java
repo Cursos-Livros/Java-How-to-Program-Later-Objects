8afe37f84c8c80e7c0f692d6b316e72d9bae7c6f
Jose Valim <joseaugustovalim1994@gmail.com>
2024-09-23 09:16:48 -0300

    feat: exemplo 7 package access

---
+++ Chapter 8 Classes Objects Depper Look/src/examples/example7packageaccess/PackageDataTest.java
@@ -0,0 +1,27 @@
+package examples.example7packageaccess;
+
+public class PackageDataTest {
+    public static void main(String[] args) {
+        PackageData packageData = new PackageData();
+
+        // output String representation of packageData
+        System.out.printf("After instantiation:%n%s%n", packageData);
+
+        // change package access data in packageData object
+        packageData.number = 77;
+        packageData.string = "Goodbye!";
+
+        // output String representation of packageData
+        System.out.printf("%nAfter changing values:%n%s%n", packageData);
+    }
+}
+
+class PackageData {
+    int number = 0;
+    String string = "Hello";
+
+    // return PackageData object String representation
+    public String toString() {
+        return String.format("number: %d; string: %s", number, string);
+    }
+}

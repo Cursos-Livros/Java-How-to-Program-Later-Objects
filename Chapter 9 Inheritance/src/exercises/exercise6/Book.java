b997b49d61567cafad03719aafc8ec0d82316bf3
Jose Valim <joseaugustovalim1994@gmail.com>
2024-11-06 12:17:15 -0300

    feat: add exercises

---
+++ Chapter 9 Inheritance/src/exercises/exercise6/Book.java
@@ -0,0 +1,13 @@
+package exercises.exercise6;
+
+public class Book {
+    private String title;
+    private Integer yearPublication;
+    private String author;
+
+    public Book(String title, Integer yearPublication, String author) {
+        this.title = title;
+        this.yearPublication = yearPublication;
+        this.author = author;
+    }
+}

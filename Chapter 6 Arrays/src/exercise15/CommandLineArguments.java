0d1ce8c32784babbd0ce434b2283d93c93a61554
JAV1994 <jose.valim@escola.pr.gov.br>
2025-05-27 12:25:50 -0300

    feat: exercise 15

--- Chapter 6 Arrays/src/exercise15/CommandLineArguments.java
+++ Chapter 6 Arrays/src/exercise15/CommandLineArguments.java
@@ -1,22 +1,44 @@
 package exercise15;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CommandLineArguments {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] commandLineList = new int[10];
+        int[] commandLineList = new int[5];
 
-        for (int element : commandLineList) {
+        for (int i = 0; i < commandLineList.length; i++) {
             System.out.println("Enter the number");
-            commandLineList[element] = input.nextInt();
+            commandLineList[i] = input.nextInt();
         }
 
         sortArray(commandLineList);
+        double average = average(commandLineList[0], commandLineList.length - 1);
+        showArray(commandLineList);
+        System.out.println("The average of the first and last elements it's: " + average);
     }
 
-    // Não use varargs não por favor
-    static void sortArray(int... commandLineList) {
 
+    static void sortArray(int... commandLineList) {
+        int temporary = 0;
+
+        for (int i = 0; i < commandLineList.length; i++) {
+            for (int j = i + 1; j < commandLineList.length; j++) {
+                if (commandLineList[j] < commandLineList[i]) {
+                    temporary = commandLineList[i];
+                    commandLineList[i] = commandLineList[j];
+                    commandLineList[j] = temporary;
+                }
+            }
+        }
+    }
+
+    static double average(int firstElement, int lastElement) {
+        return (double) (firstElement + lastElement) / 2;
+    }
+
+    static void showArray(int... commandLineList) {
+        System.out.println(Arrays.toString(commandLineList));
     }
 }

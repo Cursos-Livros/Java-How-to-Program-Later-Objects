8ee6cee1438941f2a30cf5577f04c74bfc2b47e5
JAV1994 <jose.valim@escola.pr.gov.br>
2025-05-28 09:54:49 -0300

    feat: exercise 1

--- Chapter 6 Arrays/src/exercise18/GameCraps.java
+++ Chapter 6 Arrays/src/exercise18/GameCraps.java
@@ -21,15 +21,58 @@ public class GameCraps {
         Status gameStatus; // can contain CONTINUE, WIN or LOST
 
         int sumOfDice = rollDice();
+        gameStatus = firstRoll(sumOfDice);
+
+        if (gameStatus == Status.CONTINUE) { // did not win or lose, so remember the point
+            myPoint = sumOfDice; // remember the point
+        }
+
+
+        // While game is not complete
+        while (gameStatus == Status.CONTINUE) {
+            sumOfDice = rollDice();
+
+            if (sumOfDice == myPoint) {
+                gameStatus = Status.WON;
+            } else {
+                if (sumOfDice == SEVEN) {
+                    gameStatus = Status.LOST;
+                }
+            }
+        }
+
+        // display won or lost message
+        if (gameStatus == Status.WON) {
+            System.out.println("Player wins");
+        } else {
+            System.out.println("Player loses");
+        }
+    }
+
+    // function that implements the first roll
+    public static Status firstRoll(int sumOfDice) {
+        // determine game status and point based on first roll
+        switch (sumOfDice) {
+            case SEVEN:
+            case YO_LEVEN:
+                return Status.WON;
+            case SNAKE_EYES:
+            case TREY:
+            case BOX_CARS:
+                return Status.LOST;
+            default: // did not win or lose, so remember the point
+                System.out.printf("Point is %d%n", sumOfDice);
+                return Status.CONTINUE; // game is not over
+        }
     }
 
     // roll dice calculate sum and display results
     public static int rollDice() {
         // pick random dice values
-        int dice1 = 1 + randomNumbers.nextInt(6);
-        int dice2 = 1 + randomNumbers.nextInt(6);
+        int dice1 = 1 + randomNumbers.nextInt(6); // first dice roll
+        int dice2 = 1 + randomNumbers.nextInt(6); // second dice roll
 
-        int sum = dice1 + dice2;
+        int sum = dice1 + dice2; // sum of dice values
 
         // display results of this roll
         System.out.printf("Player rolled %d + %d = %d%n", dice1, dice2, sum);

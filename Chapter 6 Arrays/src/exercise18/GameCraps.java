aafd59b1b9c353122d3931344b8bee41a1d9952c
JAV1994 <jose.valim@escola.pr.gov.br>
2025-06-02 11:34:08 -0300

    feat: exercise 17

--- Chapter 6 Arrays/src/exercise18/GameCraps.java
+++ Chapter 6 Arrays/src/exercise18/GameCraps.java
@@ -1,11 +1,12 @@
 package exercise18;
 
 import java.security.SecureRandom;
+import java.util.Arrays;
 
 public class GameCraps {
     private static final SecureRandom randomNumbers = new SecureRandom();
 
-    private enum Status {CONTINUE, WON, LOST}
+    private enum Status {CONTINUE, WON, WONFIRSTROLL, LOST}
 
     ;
 
@@ -19,51 +20,43 @@ public class GameCraps {
     public static void main(String[] args) {
         int myPoint = 0; // point if no win or loss on first roll
         Status gameStatus; // can contain CONTINUE, WIN or LOST
-
-        int sumOfDice = rollDice();
-        gameStatus = firstRoll(sumOfDice);
-
-        if (gameStatus == Status.CONTINUE) { // did not win or lose, so remember the point
-            myPoint = sumOfDice; // remember the point
-        }
+        int[] listNumberGamesWonEachRound = new int[11];
+        int[] listNumberGamesLostEachRound = new int[11];
 
 
-        // While game is not complete
-        while (gameStatus == Status.CONTINUE) {
-            sumOfDice = rollDice();
+        for (int i = 0; i < 10; i++) {
+            int roll = 0;
+            int sumOfDice = rollDice();
+            gameStatus = firstRoll(sumOfDice);
 
-            if (sumOfDice == myPoint) {
-                gameStatus = Status.WON;
+            if (gameStatus == Status.WONFIRSTROLL) {
+                listNumberGamesWonEachRound[0] += 1;
+            }
+
+            // While game is not complete
+            while (gameStatus == Status.CONTINUE) {
+                myPoint = sumOfDice;
+                gameStatus = continuePlaying(myPoint);
+                roll += 1;
+            }
+
+            int lastPositionList = listNumberGamesWonEachRound.length - 1;
+            if (roll > listNumberGamesWonEachRound.length) {
+                roll = lastPositionList;
+            }
+
+            // display won or lost message
+            if (gameStatus == Status.WON) {
+                listNumberGamesWonEachRound[roll] += 1;
             } else {
-                if (sumOfDice == SEVEN) {
-                    gameStatus = Status.LOST;
-                }
+                listNumberGamesLostEachRound[roll] += 1;
             }
         }
 
-        // display won or lost message
-        if (gameStatus == Status.WON) {
-            System.out.println("Player wins");
-        } else {
-            System.out.println("Player loses");
-        }
-    }
-
-    // function that implements the first roll
-    public static Status firstRoll(int sumOfDice) {
-        // determine game status and point based on first roll
-        switch (sumOfDice) {
-            case SEVEN:
-            case YO_LEVEN:
-                return Status.WON;
-            case SNAKE_EYES:
-            case TREY:
-            case BOX_CARS:
-                return Status.LOST;
-            default: // did not win or lose, so remember the point
-                System.out.printf("Point is %d%n", sumOfDice);
-                return Status.CONTINUE; // game is not over
-        }
+        System.out.println("Wins:");
+        showListNumberResultsEachRound(listNumberGamesWonEachRound);
+        System.out.println("Lost:");
+        showListNumberResultsEachRound(listNumberGamesLostEachRound);
     }
 
     // roll dice calculate sum and display results
@@ -72,11 +65,42 @@ public class GameCraps {
         int dice1 = 1 + randomNumbers.nextInt(6); // first dice roll
         int dice2 = 1 + randomNumbers.nextInt(6); // second dice roll
 
-        int sum = dice1 + dice2; // sum of dice values
-
-        // display results of this roll
-        System.out.printf("Player rolled %d + %d = %d%n", dice1, dice2, sum);
+        int sum = dice1 + dice2;
 
         return sum;
     }
+
+    // function that implements the first roll
+    public static Status firstRoll(int sumOfDice) {
+        // determine game status and point based on first roll
+        switch (sumOfDice) {
+            case SEVEN:
+            case YO_LEVEN:
+                return Status.WONFIRSTROLL;
+            case SNAKE_EYES:
+            case TREY:
+            case BOX_CARS:
+                return Status.LOST;
+            default: // did not win or lose, so remember the point
+                return Status.CONTINUE; // game is not over
+        }
+    }
+
+    public static Status continuePlaying(int myPoint) {
+        int sumOfDice = rollDice();
+
+        if (sumOfDice == myPoint) {
+            return Status.WON;
+        } else {
+            if (sumOfDice == SEVEN) {
+                return Status.LOST;
+            }
+        }
+        return Status.CONTINUE;
+    }
+
+    public static void showListNumberResultsEachRound(int[] listNumberResultsEachRound) {
+        System.out.println(Arrays.toString(listNumberResultsEachRound));
+    }
+
 }

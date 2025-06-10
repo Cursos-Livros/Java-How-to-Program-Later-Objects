203f65f6508f82902430c5d06efc2386e1480bf4
JAV1994 <jose.valim@escola.pr.gov.br>
2025-06-10 10:55:33 -0300

    feat: exercise 19

--- Chapter 6 Arrays/src/exercise19/AirlineReservationsSystem.java
+++ Chapter 6 Arrays/src/exercise19/AirlineReservationsSystem.java
@@ -29,9 +29,22 @@ public class AirlineReservationsSystem {
 
         showTypeClass(classOption);
 
-        while (!verifyFullBooking(seats)) {
-            assignSeat(classOption, seats);
+        if (classOption == 1) {
+            showSeatAvaiableClassVip(classOption, seats);
+        } else {
+            showSeatAvaiableClassEconomic(classOption, seats);
         }
+
+        boolean stopBook = false;
+
+        while (!stopBook && !verifyFullBooking(seats)) {
+            System.out.println("Enter with the number of the seat:");
+            int seatsOption = input.nextInt();
+
+            System.out.println("");
+        }
+
+        assignSeat(classOption, seats);
         showSeatStatus(seats);
     }
 
@@ -74,6 +87,32 @@ public class AirlineReservationsSystem {
         }
         System.out.println("Next flight leaves in 3 hours");
         return true;
+    }
+
+    public static void showSeatAvaiableClassVip(int classOption, SetSeatStatus[] seats) {
+        int vipLimitSeat = seats.length / 2;
+
+        System.out.println("Avaiable seats on VIP class:");
+        for (int i = 0; i < vipLimitSeat; i++) {
+            System.out.printf("Seat %d: %s ", (i + 1), seats[i]);
+
+            if (i % 2 == 1) { // controla quanto quebrar a linha
+                System.out.println();
+            }
+        }
+    }
+
+    public static void showSeatAvaiableClassEconomic(int classOption, SetSeatStatus[] seats) {
+        int vipLimitSeat = seats.length / 2;
+
+        System.out.println("Avaiable seats on Economic class:");
+        for (int i = 0; i < vipLimitSeat; i++) {
+            System.out.printf("Seat %d: %s ", (i + 1), seats[i]);
+
+            if (i % 2 == 1) { // controla quanto quebrar a linha
+                System.out.println();
+            }
+        }
     }
 
     public static void assignSeat(int classOption, SetSeatStatus[] seats) {

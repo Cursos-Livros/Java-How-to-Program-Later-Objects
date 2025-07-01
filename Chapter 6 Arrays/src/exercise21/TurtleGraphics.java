cc7685bd11cd107471715fd058323a8ce8fa60d1
José Augusto Valim <joseaugustovalim1994@gmail.com>
2025-06-30 21:37:43 -0300

    feat: run, chooseDirection, testDirection methods

--- Chapter 6 Arrays/src/exercise21/TurtleGraphics.java
+++ Chapter 6 Arrays/src/exercise21/TurtleGraphics.java
@@ -1,23 +1,41 @@
 package exercise21;
 
+import java.util.Scanner;
+
 public class TurtleGraphics {
     public static void main(String[] args) {
-        int[][] floor = new int[20][20];
-        inicializeFloor(floor);
-
-        int[][] commandArray = new int[3][2];
-        handleCommands(commandArray);
+        run();
     }
 
-    public static void inicializeFloor(int[][] floor) {
-        for (int i = 0; i < floor.length; i++) {
-            for (int j = 0; j < floor[i].length; j++) {
-                floor[i][j] = 0;
-            }
+    public static void run() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("You are in the position (0,0):");
+        System.out.println("Enter with a direction:");
+        int currentDirection = chooseDirection(input);
+        showCurretDirection(currentDirection);
+
+        System.out.println();
+    }
+
+    public static int chooseDirection(Scanner input) {
+
+        int direction = input.nextInt();
+
+        while (!testDirection(direction)) {
+            System.out.println("The direction is out of range:");
+            System.out.println("Enter the direction again!");
+            System.out.printf("1 = Right ↑\n");
+            System.out.printf("2 = Left →\n");
+            direction = input.nextInt();
         }
+
+        return direction - 1;
     }
 
-    public static void handleCommands(int[][] commandArray) {
-
+    public static boolean testDirection(int direction) {
+        return direction >= 1 && direction <= 4;
     }
+
+    public
 }
